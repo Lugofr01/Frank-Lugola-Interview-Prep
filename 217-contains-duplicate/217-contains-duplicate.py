b8df71c5fbc9476927cb820ca480ffaff5b62890
Frank Charles Lugola <78613296+Lugofr01@users.noreply.github.com>
2022-06-13 13:13:31 -0500

    Time: 703 ms (12.85%), Space: 24.1 MB (21.83%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -11,7 +11,7 @@ class Solution(object):
 # We could use bruteforce and the space complexity will be O(n^2) with O(1) time
 # But if we sort we only have to worry about what's adjacent to the elemtn in the list and therefore have O(nlogn) complexity 
 
-# We can further improve the time complexity by trading off the spacr complexity and this is using a hashset the space will be O(n) and time will be O(1)
+# We can further improve the time complexity by trading off the spacr complexity and this is using a hashset the space will be O(n) and time will be O(n)
         hashset =set()
     
         for n in nums:
