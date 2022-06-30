b3cf4b00edf8f799f3b744d8213c2b2d30218cb5
Frank Charles Lugola <78613296+Lugofr01@users.noreply.github.com>
2022-06-30 12:50:22 -0500

    Time: 57 ms (61.84%), Space: 14.3 MB (19.00%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,52 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        # O(n) solution will involve going through each number in array and checking if it's our target
+#         anything better than linear is a binary search solution
+# analyse graph pattern in a sorted rotated array
+        left = 0
+        right = len(nums)-1
+        
+        
+        while left<=right:
+            
+            mid =(left+right)//2
+            if target == nums[mid]:
+                return mid
+            
+#             let's check which portion of array are we in
+
+# left sorted portion
+            if nums[left]<=nums[mid]:
+                if target > nums[mid] or target <nums[left]:
+                    left = mid+1
+                    
+#then we search right since the value is not on the left sorted portion
+                else:
+                    right = mid -1
+                
+                    
+            
+                
+# right sorted portion is the else
+            else:
+                if target < nums[mid] or target>nums[right]:
+                    right =mid-1
+                    # we search left of the array
+                else:
+                    left =mid +1
+                    # we search right part of the array
+        return -1             
+                    
+                    
+                    
+        
+                
+                
+                
+
+                
+            
+            
+
+        
