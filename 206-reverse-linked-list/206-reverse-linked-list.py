37b8798d059c3222f9702aecf271e8ba61b0b327
Frank Charles Lugola <78613296+Lugofr01@users.noreply.github.com>
2022-07-02 13:54:50 -0500

    Time: 70 ms (18.32%), Space: 20.6 MB (9.06%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -3,28 +3,28 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # let's start iteratively
-        # we have two pointers pointing to previous node and current node
-        # we want to reverse the ponters
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         # let's start iteratively
+#         # we have two pointers pointing to previous node and current node
+#         # we want to reverse the ponters
 
-        previous = None
-        current = head
+#         previous = None
+#         current = head
 
-        while current:
-            # reversing the pointers
-            temporarystorage = current.next
-            # we store the current nodes temporry
+#         while current:
+#             # reversing the pointers
+#             temporarystorage = current.next
+#             # we store the current nodes temporry
 
-            current.next = previous
-            # we point to  previous node
+#             current.next = previous
+#             # we point to  previous node
 
-            previous = current
-            current = temporarystorage
+#             previous = current
+#             current = temporarystorage
 
 
-        return previous
+#         return previous
     
     
 # o(n)  time complexity, memory O(1)
@@ -35,20 +35,20 @@ class Solution:
 
 #         self.val = val
 #         self.next = next
-# class Solution:
-#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
-#         # recursive solution
+        # recursive solution
 
-#         def recursive(previous, current):
-#             if not current:
-#                 return previous
+        def recursive(previous, current):
+            if not current:
+                return previous
 
-#             tail = recursive(current, current.next)
-#             current.next = previous
+            tail = recursive(current, current.next)
+            current.next = previous
 
-#             return tail
-#         return recursive(None,head)
+            return tail
+        return recursive(None,head)
 
 
 
