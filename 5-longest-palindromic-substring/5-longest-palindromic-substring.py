2a04be01741008206279bdaee36e0746514fe64b
Frank Charles Lugola <78613296+Lugofr01@users.noreply.github.com>
2022-07-22 16:43:26 -0500

    Time: 892 ms (86.10%), Space: 13.8 MB (91.28%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,38 +1,71 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        result =""
-        resultLength = 0
+    def longestPalindrome(self, s):
+        res = ""
+        for i in range(len(s)):
+            # odd case, like "aba"
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
+            # even case, like "abba"
+            tmp = self.helper(s, i, i+1)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+
+    # get the longest palindrome, l, r are the middle indexes   
+    # from inner to outer
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1; r += 1
+        return s[l+1:r]
+
+
+
+
+
+
+
+
+
         
         
-        for i in range (len(s)):
+        
+        
+        
+        
+#         result =""
+#         resultLength = 0
+        
+        
+#         for i in range (len(s)):
             
-            #odd length edge case
-            left,right= i,i
-            # i is the center 
-            while left>=0 and right <len(s) and s[left]==s[right]:
-                # we check if the result length
-                if (right-left +1)>resultLength:
-                    result = s[left:right+1]
-                    resultLength = right-left +1
-                left-=1
-                right+=1
+#             #odd length edge case
+#             left,right= i,i
+#             # i is the center 
+#             while left>=0 and right <len(s) and s[left]==s[right]:
+#                 # we check if the result length
+#                 if (right-left +1)>resultLength:
+#                     result = s[left:right+1]
+#                     resultLength = right-left +1
+#                 left-=1
+#                 right+=1
                 
-            #even length edge case
-            left,right= i,i+1
-            # i is the center 
-            while left>=0 and right <len(s) and s[left]==s[right]:
-                # we check if the result length
-                if (right-left +1)>resultLength:
-                    result = s[left:right+1]
-                    resultLength = right-left +1
-                left-=1
-                right+=1
+#             #even length edge case
+#             left,right= i,i+1
+#             # i is the center 
+#             while left>=0 and right <len(s) and s[left]==s[right]:
+#                 # we check if the result length
+#                 if (right-left +1)>resultLength:
+#                     result = s[left:right+1]
+#                     resultLength = right-left +1
+#                 left-=1
+#                 right+=1
                 
-        return result
+#         return result
                 
             
                 
-            # complexity is 0(n^3)
+#             # complexity is 0(n^3)
 
             
         
