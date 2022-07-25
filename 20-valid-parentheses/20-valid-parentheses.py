4a07077c0870eba0afe948c1008c13b5eb33297e
Frank Charles Lugola <78613296+Lugofr01@users.noreply.github.com>
2022-07-25 11:58:54 -0500

    Time: 61 ms (19.68%), Space: 13.9 MB (70.03%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -7,10 +7,14 @@ class Solution:
             if parantheses in dic.values():
                 stack.append(parantheses)
             elif stack and dic[parantheses]==stack[-1]:
+                # stack[-1] means stack at the top  
                 stack.pop()
                 
             else:
                 return False
         return stack ==[]
+    
+    
+    # 0(N) time and o(n) space
         
         
