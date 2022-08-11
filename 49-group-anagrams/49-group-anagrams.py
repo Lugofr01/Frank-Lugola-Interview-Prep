fe5cfc7e0ee82a15c8e0ae7663ab95244a4c1abe
Frank Charles Lugola <78613296+Lugofr01@users.noreply.github.com>
2022-08-11 13:01:58 -0500

    Time: 222 ms (14.96%), Space: 19.9 MB (21.42%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,18 +1,41 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        lookup = defaultdict(list)
         
-        for word in strs:
+        result = defaultdict(list) #hashmap for mapping character count 
+        
+        for string in strs:
+            count =[0]*26 #a.......z
+            
+            
+            for char in string:
+                
+# we go through ecvery character in the string
+                count[ord(char)- ord("a")] +=1
+            result[tuple(count)].append(string) 
+        
+        
+        
+        
+        return result.values()
+           
+                
+            
+        # complexity o(m*n)
+        
+        
+#         lookup = defaultdict(list)
+        
+#         for word in strs:
                
             
             
-            key = "".join (sorted(list(word)))
-            lookup[key].append(word)
-        output=[]                   
-        for lists in lookup.values():
-            output.append(lists)
+#             key = "".join (sorted(list(word)))
+#             lookup[key].append(word)
+#         output=[]                   
+#         for lists in lookup.values():
+#             output.append(lists)
                            
-        return output
+#         return output
             
         
         
